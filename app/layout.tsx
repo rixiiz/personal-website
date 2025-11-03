b960b4bb5ba078a621c3305ffa59ffa243e1ffd9
rixiiz <135349296+rixiiz@users.noreply.github.com>
2025-11-03 14:28:41 -0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,36 +1,35 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
 import portfolioData from "@/lib/config";
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: `${portfolioData.name}'s Personal Website`,
-	description: `All about ${portfolioData.name}`,
+  title: `${portfolioData.name}'s Personal Website`,
+  description: `All about ${portfolioData.name}`,
 };
 
 export default function RootLayout({
-	children,
-}: Readonly<{
-	children: React.ReactNode;
-}>) {
-	return (
-		<html lang="en">
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable}`}
+      suppressHydrationWarning
+    >
+      <body className="antialiased">{children}</body>
+    </html>
+  );
 }
